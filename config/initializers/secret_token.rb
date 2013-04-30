5c61a8c15330caeb35afb1b5602b598e3d2f5491
vagrant <jeanfbrito@gmail.com>
2013-04-30 12:20:10 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Judi::Application.config.secret_token = '93f7a5191e1a2657c9804d049ddd74b5048d737184ffa34770b4064361117920e9fecf87bcceff3210820eacdf13fa34d51ee83fd19f397ac569b828628bfcbe'
